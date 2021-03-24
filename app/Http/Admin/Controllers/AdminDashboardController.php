b6fb8a0807b40032c61962cc143fbe88cbbec78b
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-03-24 20:32:48 +0300

    update
    update

--- app/Http/Admin/Controllers/AdminDashboardController.php
+++ app/Http/Admin/Controllers/AdminDashboardController.php
@@ -20,7 +20,23 @@ class AdminDashboardController extends Controller
     public function index()
 
     {
+        $today_users = User::whereDate('created_at', today())->count();
+        $yesterday_users = User::whereDate('created_at', today()->subDays(1))->count();
+        $users_2_days_ago = User::whereDate('created_at', today()->subDays(2))->count();
+        // Billing
+        $today_billings = DB::table('subscriptions')->whereDate('created_at', today())->count();
+        $yesterday_billings = DB::table('subscriptions')->whereDate('created_at', today()->subDays(1))->count();
+        $billings_2_days_ago = DB::table('subscriptions')->whereDate('created_at', today()->subDays(2))->count();
         
+        // Visitors log graph
+        $today_visitors = DB::table('visitlogs')->whereDate('updated_at', today())->count();
+        $yesterday_visitors = DB::table('visitlogs')->whereDate('updated_at', today()->subDays(1))->count();
+        $visitors_2_days_ago = DB::table('visitlogs')->whereDate('updated_at', today()->subDays(2))->count();
+
+        $chart = new UsersChart;
+        $chart->labels(['2 days ago', 'Yesterday', 'Today']);
+        $chart->dataset('User Registration', 'line', [$users_2_days_ago, $yesterday_users, $today_users]);
+
         $chart->dataset('User Subscription', 'line', [$billings_2_days_ago, $yesterday_billings, $today_billings])->options([
             'color' => '#ff0000',
             'backgroundColor'=> 'rgba(240, 127, 110, 0.3)',
@@ -35,7 +51,16 @@ class AdminDashboardController extends Controller
     }
 
     public function statistics(){
-       
+        // Get all the users
+        $user_count = User::all()->count();
+        $newTicket = Ticket::where('status', 'Open')->count();
+        $unique_visitors = DB::table('visitlogs')->get()->count();
+        $total_subscription = DB::table('subscriptions')->get()->count();
+        return ['users' => $user_count, 
+                'newTicket' => $newTicket, 
+                'unique_visitors' => $unique_visitors,
+                'total_subscription' => $total_subscription
+                ];
 
     }
 
