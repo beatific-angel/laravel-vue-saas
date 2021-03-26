7ac3b4c6a31d98b1debe5438ef3a05ec4bb08afa
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-03-26 20:34:09 +0300

    update
    update

--- resources/views/account/dashboard/index.blade.php
+++ resources/views/account/dashboard/index.blade.php
@@ -46,33 +46,7 @@
                                 aria-valuemax="100" style="width: 50%;"></div>
                         </div>
                     </div>
-                    <div class="col-auto">
-                        <button type="button" class="btn btn-sm btn-neutral mr-0" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-h"></i>
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-right">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
-                <p class="mt-3 mb-0 text-sm">
-                    <a href="#!" class="text-nowrap text-white font-weight-600">See details</a>
-                </p>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-md-6">
-        <div class="card bg-gradient-danger border-0">
-            <!-- Card body -->
-            <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        <h5 class="card-title text-uppercase text-muted mb-0 text-white">Items sold</h5>
-                        <span class="h2 font-weight-bold mb-0 text-white">5622</span>
-                        <div class="progress progress-xs mt-3 mb-0">
+                   
                             <div class="progress-bar bg-success" role="progressbar" aria-valuenow="80" aria-valuemin="0"
                                 aria-valuemax="100" style="width: 80%;"></div>
                         </div>
