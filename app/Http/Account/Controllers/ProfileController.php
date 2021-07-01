a75a80699c6863b02bea36932980db1eb9e8c1ce
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2021-07-01 11:43:35 +0300

    update
    update

--- app/Http/Account/Controllers/ProfileController.php
+++ app/Http/Account/Controllers/ProfileController.php
@@ -39,22 +39,14 @@ class ProfileController extends Controller
 
         // Check if a profile image has been uploaded
         if ($request->has('profile_image')) {
-            // Get image file
             $image = $request->file('profile_image');
-            // Make a image name based on user name and current timestamp
             $name = str::slug($request->input('name')).'_'.time();
-            // Define folder path
             $folder = '/uploads/images/';
-            // Make a file path where image will be stored [ folder path + file name + file extension]
             $filePath = $folder . $name. '.' . $image->getClientOriginalExtension();
-            // Upload image
             $this->uploadOne($image, $folder, 'public', $name);
-            // Set user profile image path in database to filePath
             $user->profile_image = $filePath;
         }
-        // Persist user record to database
         $user->save();
-        //redirect with success
         return back()->withSuccess('Profile updated successfully.');
     }
 }
