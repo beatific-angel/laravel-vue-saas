5de3929adc62de6b544f3d69e0bf9a86dc194272
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2022-01-17 04:47:50 +0300

    update
    update

--- resources/views/account/dashboard/index.blade.php
+++ resources/views/account/dashboard/index.blade.php
@@ -7,29 +7,7 @@
             <!-- Card body -->
             <div class="card-body">
                 <div class="row">
-                    <div class="col">
-                        <h5 class="card-title text-uppercase text-muted mb-0 text-white">Tickets</h5>
-                        <span class="h2 font-weight-bold mb-0 text-white">0</span>
-                        <div class="progress progress-xs mt-3 mb-0">
-                            <div class="progress-bar bg-success" role="progressbar" aria-valuenow="30" aria-valuemin="0"
-                                aria-valuemax="100" style="width: 30%;"></div>
-                        </div>
-                    </div>
-                    <div class="col-auto">
-                        <button type="button" class="btn btn-sm btn-neutral mr-0" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-h"></i>
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-right">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
-                <p class="mt-3 mb-0 text-sm">
-                    <a href="{{ route('ticket.index') }}" class="text-nowrap text-white font-weight-600">Go to tickets</a>
-                </p>
+                    
             </div>
         </div>
     </div>
